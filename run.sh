b22705e94a89f4f2f8f899482d93801fc1c13348
Nathanael Sensfelder <spamshield0@noot-noot.org>
2023-07-20 12:33:58 +0200

    Will now read regex params from files.

--- run.sh
+++ run.sh
@@ -5,6 +5,6 @@ SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 while true
 do
    rm -f $1
-   $SCRIPT_DIR/jabberhive-regex.py -s $1 -t $2 -f $3 -r $4 -x $5
+   $SCRIPT_DIR/jabberhive-regex.py -s $1 -t $2 -f $3 -r "$(cat $SCRIPT_DIR/capture_pattern.txt)" -x "$(cat $SCRIPT_DIR/replace_pattern.txt)"
    sleep 60
 done
